2111bedfc75e82d1bd8bbff1e588eb98cd56767a
a.firsova <a.firsova@propellerads.net>
2025-10-28 13:31:57 +0300

    added open page tests

---
+++ test/open_page_test.py
@@ -0,0 +1,30 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture
+def driver():
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--window-size=1280,900")
+
+    driver = webdriver.Chrome(options=options)
+    yield driver
+    driver.quit()
+
+
+def test_github_open(driver):
+    url = "https://github.com/"
+    driver.get(url)
+
+    assert "GitHub" in driver.title
+    assert driver.current_url == url
+
+
+def test_selenium_open(driver):
+    url = "https://www.selenium.dev/"
+    driver.get(url)
+
+    assert "Selenium" in driver.title
+    assert driver.current_url == url
