3a01321944a70c8dd9fd8e86109549abac9d8337
a.firsova <a.firsova@propellerads.net>
2025-10-28 13:32:09 +0300

    added search test

--- test/search_test.py
+++ test/search_test.py
@@ -1,10 +1,38 @@
+import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
-def test_google_search():
-    driver = webdriver.Chrome()
+@pytest.fixture
+def driver():
+    options = Options()
+    options.add_argument("--window-size=1280,900")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(options=options)
+    yield driver
+    driver.quit()
+
+
+def test_google_search(driver):
     url = "https://www.google.com/"
     driver.get(url)
+    search_string = "pytest selenium"
 
     assert driver.title == "Google"
     assert driver.current_url == url
+
+    element = driver.find_element(By.NAME, "q")
+    element.send_keys(search_string)
+    element.submit()
+
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "#search"))
+    )
+
+    assert search_string in driver.page_source
